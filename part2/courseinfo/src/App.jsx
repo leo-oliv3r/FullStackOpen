cfac81f53089d39f6be5f46f019c8ebf47f9594b
lvic3nt <leonardo.vic3nte@gmail.com>
2024-01-21 20:58:17 -0300

    finish part 2.3 and 2.4 and 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -3,32 +3,52 @@
 import Course from "./components/Course/Course";
 
 function App() {
-    const course = {
-        id: 1,
-        title: "Half Stack application development",
+    const courses = [
+        {
+            id: 1,
+            title: "Half Stack application development",
 
-        courseParts: [
-            {
-                name: "Fundamentals of React",
-                nrExercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                nrExercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                nrExercises: 14,
-                id: 3,
-            },
-        ],
-    };
+            courseParts: [
+                {
+                    name: "Fundamentals of React",
+                    nrExercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    nrExercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    nrExercises: 14,
+                    id: 3,
+                },
+            ],
+        },
+        {
+            id: 2,
+            title: "Node.js",
+            courseParts: [
+                {
+                    name: "Routing",
+                    nrExercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    nrExercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
     return (
         <div style={{ fontFamily: "sans-serif" }}>
-            <Course course={course} />
+            {courses.map((course) => (
+                <Course key={course.id} course={course} />
+            ))}
         </div>
     );
 }
