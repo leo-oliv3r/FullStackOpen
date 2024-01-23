d0a73dd889a34dd095be99b25e43da485d71ca06
lvic3nt <leonardo.vic3nte@gmail.com>
2024-01-22 21:30:57 -0300

    finish part 2.7

--- part2/phonebook/src/components/ContactForm.jsx
+++ part2/phonebook/src/components/ContactForm.jsx
@@ -3,8 +3,15 @@
 function ContactForm({ setNewName, newName, setPersons, persons }) {
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (persons.find((person) => person.name.toLowerCase() === newName.toLowerCase())) {
+            alert(`${newName} is already added to phonebook`);
+            return;
+        }
+
         const newPersons = [...persons, { name: newName }];
         setPersons(newPersons);
+        setNewName("");
     };
 
     return (
