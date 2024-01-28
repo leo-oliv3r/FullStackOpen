3c7efb0ee9c9832a0a7ab6c64f2306d86d41ef8c
lvic3nt <leonardo.vic3nte@gmail.com>
2024-01-27 23:14:41 -0300

    finish part 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,75 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
+import countriesService from "./services/countries";
 
-function App() {}
+function Country({ country, detailed }) {
+    if (detailed) {
+        return (
+            <>
+                <h3>{country.name.common}</h3>
+                <p>Capital: {country.capital[0]}</p>
+                <p>Area: {country.area}</p>
+                <div>
+                    Languages:
+                    <ul>
+                        {Object.entries(country.languages).map((language) => (
+                            <li key={language}>{language[1]}</li>
+                        ))}
+                    </ul>
+                </div>
+                <img src={country.flags.png} alt={country.flags.alt} />
+            </>
+        );
+    }
+
+    return <p>{country.name.common}</p>;
+}
+
+function CountryList({ countries }) {
+    if (countries.length > 10) {
+        return <p>Too many countries found, be more specific</p>;
+    }
+
+    if (countries.length === 1) {
+        return <Country country={countries[0]} detailed={true} />;
+    }
+
+    return countries.map((country) => <p key={country.name.common}>{country.name.common}</p>);
+}
+
+function Search({ children, onChange }) {
+    return (
+        <>
+            <label htmlFor="search">{children}</label>
+            <input type="text" id="search" onChange={onChange} />
+        </>
+    );
+}
+
+function App() {
+    const [countries, setCountries] = useState(null);
+    const [countriesToRender, setCountriesToRender] = useState([]);
+
+    useEffect(() => {
+        countriesService.getAllCountries().then((res) => {
+            setCountries(res);
+        });
+    }, []);
+
+    function handleInputChange(event) {
+        const currentInputValue = event.target.value;
+        const countriesFound = countries.filter((country) =>
+            country.name.common.toLowerCase().includes(currentInputValue.toLowerCase())
+        );
+        setCountriesToRender(countriesFound);
+    }
+
+    return (
+        <div style={{ fontFamily: "sans-serif" }}>
+            {countries && <Search onChange={(e) => handleInputChange(e)}>Search country: </Search>}
+            <CountryList countries={countriesToRender}></CountryList>
+        </div>
+    );
+}
 
 export default App;
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/persons";
+const BASE_URL = "https://studies.cs.helsinki.fi/restcountries/api";
+
+async function getAllCountries() {
+    return await axios.get(`${BASE_URL}/all`).then((res) => res.data);
+}
+
+export default { getAllCountries };
