fe58d8d276e76ee37e8f7f1039cc0f6b8c006837
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-02-10 23:24:25 -0300

    wip

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const argc = process.argv.length;
+
+if (argc !== 5) {
+    console.log("Usage: node mongo.js <password> <ContactName> <ContactNumber>");
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://leonardovic3nte:${password}@cluster0.zgevewk.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const contactModel = new mongoose.model("Contact", contactSchema);
+
