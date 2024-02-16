32baf0d76966d24f4181a882dd00caab746bf0da
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-02-15 22:44:33 -0300

    some cleanup

--- part3/phonebook/backend/.eslintrc.cjs
+++ part3/phonebook/backend/.eslintrc.cjs
@@ -1,27 +1,27 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  extends: "eslint:recommended",
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["no-floating-promise", "promise"],
-  rules: {
-    "no-floating-promise/no-floating-promise": 2,
-    "promise/catch-or-return": "error",
-    "promise/always-return": "error",
-  },
+	env: {
+		node: true,
+		es2021: true,
+	},
+	extends: ["airbnb-base", "prettier"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		"no-console": "off",
+		"import/extensions": ["error", "ignorePackages"],
+		"no-underscore-dangle": "off",
+	},
 };
--- part3/phonebook/frontend/.eslintrc.cjs
+++ part3/phonebook/frontend/.eslintrc.cjs
@@ -1,20 +1,27 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	env: {
+		node: true,
+		es2021: true,
+	},
+	extends: ["airbnb-base", "prettier"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		"no-console": "off",
+		"import/extensions": ["error", "ignorePackages"],
+		"no-underscore-dangle": "off",
+	},
+};
