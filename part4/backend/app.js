9ac43ca86a249076308ec39f228cf1b056c8314e
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-02-25 22:36:09 -0300

    finish part 4.1 and 4.2

---
+++ part4/backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import config from "./utils/config.js";
+import blogRouter from "./controllers/blogs.js";
+import logger from "./utils/logger.js";
+
+const app = express();
+
+try {
+  logger.info("connecting to database...");
+  await mongoose.connect(config.MONGODB_URI);
+  logger.info("successfully connected to database");
+} catch (error) {
+  logger.error(error.message);
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+
+export default app;
