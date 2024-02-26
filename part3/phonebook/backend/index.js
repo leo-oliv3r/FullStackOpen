999f6b962dcf62faf6286e614f8448a6e7f39402
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-02-25 21:51:48 -0300

    remove bookmark comments

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,8 +2,6 @@ import app from "./app.js";
 import logger from "./utils/logger.js";
 import config from "./utils/config.js";
 
-// @todo Once integration with frontend is done, deploy to fly.io
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
