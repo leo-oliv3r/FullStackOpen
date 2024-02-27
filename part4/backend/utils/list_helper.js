6b374e041070f1ebdc8b155856a82e344fa0c0bf
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-02-26 22:12:02 -0300

    finish part 4.5

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -6,4 +6,15 @@ function getSumOfLikes(blogs) {
   return blogs.map((blog) => blog.likes).reduce((acc, current) => acc + current, 0);
 }
 
-export { dummy, getSumOfLikes };
+function getFavoriteBlog(blogs) {
+  if (blogs.length === 0) return null;
+
+  const highestNrOfLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const mostLikedBlog = blogs.find((blog) => blog.likes === highestNrOfLikes);
+
+  const { title, author, likes } = mostLikedBlog;
+
+  return { title, author, likes };
+}
+
+export { dummy, getSumOfLikes, getFavoriteBlog };
