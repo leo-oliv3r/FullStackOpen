ffc103f3f0ba53099be645f114c54d2bd2ae205d
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-03-09 23:11:54 -0300

    handle different dbs for test and prod

--- part4/backend/utils/config.js
+++ part4/backend/utils/config.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 const { PORT } = process.env;
-const { MONGODB_URI } = process.env;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
 export default { PORT, MONGODB_URI };
