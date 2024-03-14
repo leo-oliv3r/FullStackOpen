2bc298164ad33e03b05067cfb0f79bc8db92af7c
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-03-13 23:52:39 -0300

    wip

--- part4/backend/controllers/blogController.test.js
+++ part4/backend/controllers/blogController.test.js
@@ -1,25 +1,36 @@
-// import { beforeEach, describe, test } from "node:test";
-// import assert from "node:assert";
-// import supertest from "supertest";
-// import app from "../app.js";
-// import Blog from "../models/Blog.js";
+import { beforeEach, describe, test, after } from "node:test";
+import mongoose from "mongoose";
+import assert from "node:assert";
+import supertest from "supertest";
+import app from "../app.js";
+import Blog from "../models/Blog.js";
 
-// const api = supertest(app);
+const api = supertest(app);
 
-// beforeEach(async () => {
-//   await Blog.deleteMany({});
-//   const blogs = [
-//     { title: "Blog 1", author: "Author 1", url: "https://example.com/blog1" },
-//     { title: "Blog 2", author: "Author 2", url: "https://example.com/blog2" },
-//   ];
-//   await Blog.insertMany(blogs);
-// });
+const initialBlogsData = [
+  { title: "Blog 1", author: "Author 1", url: "https://example.com/blog1" },
+  { title: "Blog 2", author: "Author 2", url: "https://example.com/blog2" },
+  { title: "Blog 3", author: "Author 3", url: "https://example.com/blog3" },
+  { title: "Blog 4", author: "Author 4", url: "https://example.com/blog4" },
+  { title: "Blog 5", author: "Author 5", url: "https://example.com/blog5" },
+];
 
-// // describe("GET /blogs", () => {
-// //   test("should return all blogs", async () => {
-// //     const response = await api.get("/blogs").expect(200);
-// //     assert.strictEqual(response.body.length, 2);
-// //     assert.strictEqual(response.body[0].title, "Blog 1");
-// //     assert.strictEqual(response.body[1].title, "Blog 2");
-// //   });
-// // });
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const savePromises = initialBlogsData.map((blog) => new Blog(blog)).map((blog) => blog.save());
+  await Promise.all(savePromises);
+});
+
+describe("GET", () => {
+  describe("/blogs", () => {
+    test("should return all blogs", async () => {
+      const response = await api.get("/api/blogs").expect(200);
+      const { body } = response;
+      assert.strictEqual(body.length, initialBlogsData.length);
+    });
+  });
+});
+
+// @todo write rest of test and implementations
+
+after(async () => mongoose.connection.close());
