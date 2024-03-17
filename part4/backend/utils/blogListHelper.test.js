f31a3beb275eaa4e3c30f21caafa6e41ce9de05e
leo-oliv3r <leonardo.vic3nte@gmail.com>
2024-03-17 00:36:18 -0300

    add describe to list helper functions tests

--- part4/backend/utils/blogListHelper.test.js
+++ part4/backend/utils/blogListHelper.test.js
@@ -45,68 +45,70 @@ function getRandomBlog(blogsArray) {
   return blogsArray[Math.floor(Math.random() * blogsArray.length)];
 }
 
-describe("getSumOfLikes", () => {
-  test("return 0 with no blogs given", () => {
-    const emptyBlogsList = [];
-    assert.strictEqual(getSumOfLikes(emptyBlogsList), 0);
+describe("blog list helper functions", () => {
+  describe("getSumOfLikes", () => {
+    test("return 0 with no blogs given", () => {
+      const emptyBlogsList = [];
+      assert.strictEqual(getSumOfLikes(emptyBlogsList), 0);
+    });
+
+    test("return correct number of likes likes for one blog", () => {
+      const singleBlog = [getRandomBlog(blogsTestData)];
+      const { likes } = singleBlog[0];
+      assert.strictEqual(getSumOfLikes(singleBlog), likes);
+    });
+
+    test("return correct number of likes likes for many blogs", () => {
+      const totalLikesInBlogsTestData = 36;
+      assert.strictEqual(getSumOfLikes(blogsTestData), totalLikesInBlogsTestData);
+    });
   });
 
-  test("return correct number of likes likes for one blog", () => {
-    const singleBlog = [getRandomBlog(blogsTestData)];
-    const { likes } = singleBlog[0];
-    assert.strictEqual(getSumOfLikes(singleBlog), likes);
+  describe("getFavoriteBlog", () => {
+    test("return null with no blogs given", () => {
+      const emptyBlogsList = [];
+      assert.strictEqual(getFavoriteBlog(emptyBlogsList), null);
+    });
+
+    test("return correctly formatted data for two blog list", () => {
+      const sampleBlogs = [
+        { title: "blog1", author: "author1", likes: 5 },
+        { title: "blog2", author: "author2", likes: 6 },
+      ];
+      assert.deepStrictEqual(getFavoriteBlog(sampleBlogs), sampleBlogs[1]);
+    });
+
+    test("return correctly formatted data for many blogs list", () => {
+      const blogWithMostLikes = blogsTestData[2];
+      const sut = getFavoriteBlog(blogsTestData);
+
+      assert.strictEqual(sut?.author, blogWithMostLikes.author);
+      assert.strictEqual(sut?.title, blogWithMostLikes.title);
+      assert.strictEqual(sut?.likes, blogWithMostLikes.likes);
+    });
   });
 
-  test("return correct number of likes likes for many blogs", () => {
-    const totalLikesInBlogsTestData = 36;
-    assert.strictEqual(getSumOfLikes(blogsTestData), totalLikesInBlogsTestData);
-  });
-});
+  describe("getAuthorWithMostBlogs", () => {
+    test("return null with empty blogs list", () => {
+      const emptyBlogsList = [];
+      assert.strictEqual(getAuthorWithMostBlogs(emptyBlogsList), null);
+    });
 
-describe("getFavoriteBlog", () => {
-  test("return null with no blogs given", () => {
-    const emptyBlogsList = [];
-    assert.strictEqual(getFavoriteBlog(emptyBlogsList), null);
-  });
+    test("return correct author with one blog", () => {
+      const testData = [{ author: "Edsger W. Dijkstra", nrBlogs: 1 }];
+      const expected = {
+        author: "Edsger W. Dijkstra",
+        nrBlogs: 1,
+      };
+      assert.deepStrictEqual(getAuthorWithMostBlogs(testData), expected);
+    });
 
-  test("return correctly formatted data for two blog list", () => {
-    const sampleBlogs = [
-      { title: "blog1", author: "author1", likes: 5 },
-      { title: "blog2", author: "author2", likes: 6 },
-    ];
-    assert.deepStrictEqual(getFavoriteBlog(sampleBlogs), sampleBlogs[1]);
-  });
-
-  test("return correctly formatted data for many blogs list", () => {
-    const blogWithMostLikes = blogsTestData[2];
-    const sut = getFavoriteBlog(blogsTestData);
-
-    assert.strictEqual(sut?.author, blogWithMostLikes.author);
-    assert.strictEqual(sut?.title, blogWithMostLikes.title);
-    assert.strictEqual(sut?.likes, blogWithMostLikes.likes);
-  });
-});
-
-describe("getAuthorWithMostBlogs", () => {
-  test("return null with empty blogs list", () => {
-    const emptyBlogsList = [];
-    assert.strictEqual(getAuthorWithMostBlogs(emptyBlogsList), null);
-  });
-
-  test("return correct author with one blog", () => {
-    const testData = [{ author: "Edsger W. Dijkstra", nrBlogs: 1 }];
-    const expected = {
-      author: "Edsger W. Dijkstra",
-      nrBlogs: 1,
-    };
-    assert.deepStrictEqual(getAuthorWithMostBlogs(testData), expected);
-  });
-
-  test("return correct author with multiple blogs", () => {
-    const expected = {
-      author: "Robert C. Martin",
-      nrBlogs: 3,
-    };
-    assert.deepStrictEqual(getAuthorWithMostBlogs(blogsTestData), expected);
+    test("return correct author with multiple blogs", () => {
+      const expected = {
+        author: "Robert C. Martin",
+        nrBlogs: 3,
+      };
+      assert.deepStrictEqual(getAuthorWithMostBlogs(blogsTestData), expected);
+    });
   });
 });
