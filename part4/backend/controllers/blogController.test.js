7ad59c20e75551746a1fa5e8fc4636fff2f0ac43
leo.vi <leonardo.vic3nte@gmail.com>
2024-06-02 11:05:17 -0300

    add a describe to separate controllers tests on log

--- part4/backend/controllers/blogController.test.js
+++ part4/backend/controllers/blogController.test.js
@@ -41,145 +41,147 @@ beforeEach(async () => {
   await Promise.all(savePromises);
 });
 
-describe("GET", () => {
-  describe("/api/blogs", () => {
-    test("return all blogs", async () => {
-      const response = await api.get(BLOGS_URI).expect(200);
-      const fetchedBlogs = response.body;
-      assert.strictEqual(fetchedBlogs.length, initialBlogsData.length);
+describe("BLOG CONTROLLER", () => {
+  describe("GET", () => {
+    describe("/api/blogs", () => {
+      test("return all blogs", async () => {
+        const response = await api.get(BLOGS_URI).expect(200);
+        const fetchedBlogs = response.body;
+        assert.strictEqual(fetchedBlogs.length, initialBlogsData.length);
+      });
+
+      test("return blogs with correct format and shape", async () => {
+        const response = await api.get(BLOGS_URI).expect(200);
+        const fetchedBlogs = response.body;
+        fetchedBlogs.forEach((blog) => assertBlogShape(blog));
+      });
     });
 
-    test("return blogs with correct format and shape", async () => {
-      const response = await api.get(BLOGS_URI).expect(200);
-      const fetchedBlogs = response.body;
-      fetchedBlogs.forEach((blog) => assertBlogShape(blog));
-    });
-  });
-
-  describe("/api/blogs/:id", () => {
-    test("return 200 and the note with the specified id if it exists", async () => {
-      const firstBlogInDB = await Blog.findOne({});
-      // @ts-ignore
-      const { id } = firstBlogInDB;
-
-      const response = await api
-        .get(`${BLOGS_URI}/${id}`)
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
-
-      const blogFound = response.body;
-
-      assertBlogShape(blogFound);
-    });
-  });
-});
-
-describe("POST", () => {
-  const validInput = {
-    title: "Title",
-    author: "Author",
-    url: "url",
-  };
-
-  // Dont conform to blogSchema validation
-  const invalidInput = {
-    title: "ts",
-    author: "as",
-  };
-
-  describe("/api/blogs", () => {
-    test("given valid input, correctly create blog with 201 response", async () => {
-      const initialBlogs = await Blog.find({});
-
-      await api
-        .post(BLOGS_URI)
-        .send(validInput)
-        .expect(201)
-        .expect("Content-Type", /application\/json/);
-
-      const updatedBlogs = await Blog.find({});
-
-      assert(updatedBlogs.length === initialBlogs.length + 1);
-    });
-
-    test("correctly add default data if none provided", async () => {
-      const response = await api
-        .post(BLOGS_URI)
-        .send(validInput)
-        .expect(201)
-        .expect("Content-Type", /application\/json/);
-      const createdNote = response.body;
-      assert(Object.prototype.hasOwnProperty.call(createdNote, "likes"));
-    });
-
-    test("correctly rejects given data that does not conform to schema", async () => {
-      const response = await api.post(BLOGS_URI).send(invalidInput).expect(400);
-      assert(response.body.error.includes("validation failed"));
-    });
-  });
-});
-
-describe("DELETE", () => {
-  describe("api/blogs/:id", () => {
-    test("given valid id, correctly delete blog", async () => {
-      const blogsBeforeDelete = await Blog.find({});
-      const validId = blogsBeforeDelete[0].id;
-
-      await api.delete(`${BLOGS_URI}/${validId}`).expect(204);
-      const blogsAfterDelete = await Blog.find({});
-
-      assert(blogsAfterDelete.length === blogsBeforeDelete.length - 1);
-    });
-
-    test("given valid id, but no blog found, return 404", async () => {
-      const randomValidId = generateValidMongooseId();
-      await api.delete(`${BLOGS_URI}/${randomValidId}`).expect(404);
-    });
-
-    test("given invalid id return 400", async () => {
-      const invalidId = "invalidId";
-      await api.delete(`${BLOGS_URI}/${invalidId}`).expect(400);
-    });
-  });
-});
-
-describe("PUT", () => {
-  describe("/api/blogs/:id", () => {
-    test("given valid id, correctly update blog", async () => {
-      const firstBlogInDB = await Blog.findOne({});
-      const validId = firstBlogInDB?.id;
-
-      const updatedData = {
-        title: "Updated Blog",
-        author: "Updated Author",
-        url: "https://example.com/updated-blog",
-      };
-
-      const response = await api
+    describe("/api/blogs/:id", () => {
+      test("return 200 and the note with the specified id if it exists", async () => {
+        const firstBlogInDB = await Blog.findOne({});
         // @ts-ignore
-        .put(`${BLOGS_URI}/${validId}`)
-        .send(updatedData)
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
+        const { id } = firstBlogInDB;
 
-      assertBlogShape(response.body);
+        const response = await api
+          .get(`${BLOGS_URI}/${id}`)
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
+
+        const blogFound = response.body;
+
+        assertBlogShape(blogFound);
+      });
     });
+  });
 
-    test("given valid id, but no blog found, return 404", async () => {
-      const validId = generateValidMongooseId();
-      const updatedData = {
-        title: "Updated Blog",
-        author: "Updated Author",
-        url: "https://example.com/updated-blog",
-      };
+  describe("POST", () => {
+    const validInput = {
+      title: "Title",
+      author: "Author",
+      url: "url",
+    };
 
-      await api.put(`${BLOGS_URI}/${validId}`).send(updatedData).expect(404);
+    // Dont conform to blogSchema validation
+    const invalidInput = {
+      title: "ts",
+      author: "as",
+    };
+
+    describe("/api/blogs", () => {
+      test("given valid input, correctly create blog with 201 response", async () => {
+        const initialBlogs = await Blog.find({});
+
+        await api
+          .post(BLOGS_URI)
+          .send(validInput)
+          .expect(201)
+          .expect("Content-Type", /application\/json/);
+
+        const updatedBlogs = await Blog.find({});
+
+        assert(updatedBlogs.length === initialBlogs.length + 1);
+      });
+
+      test("correctly add default data if none provided", async () => {
+        const response = await api
+          .post(BLOGS_URI)
+          .send(validInput)
+          .expect(201)
+          .expect("Content-Type", /application\/json/);
+        const createdNote = response.body;
+        assert(Object.prototype.hasOwnProperty.call(createdNote, "likes"));
+      });
+
+      test("correctly rejects given data that does not conform to schema", async () => {
+        const response = await api.post(BLOGS_URI).send(invalidInput).expect(400);
+        assert(response.body.error.includes("validation failed"));
+      });
     });
+  });
 
-    test("given invalid id, return 400", async () => {
-      const invalidId = "invalid-id";
+  describe("DELETE", () => {
+    describe("api/blogs/:id", () => {
+      test("given valid id, correctly delete blog", async () => {
+        const blogsBeforeDelete = await Blog.find({});
+        const validId = blogsBeforeDelete[0].id;
 
-      await api.put(`${BLOGS_URI}/${invalidId}`).send({}).expect(400);
+        await api.delete(`${BLOGS_URI}/${validId}`).expect(204);
+        const blogsAfterDelete = await Blog.find({});
+
+        assert(blogsAfterDelete.length === blogsBeforeDelete.length - 1);
+      });
+
+      test("given valid id, but no blog found, return 404", async () => {
+        const randomValidId = generateValidMongooseId();
+        await api.delete(`${BLOGS_URI}/${randomValidId}`).expect(404);
+      });
+
+      test("given invalid id return 400", async () => {
+        const invalidId = "invalidId";
+        await api.delete(`${BLOGS_URI}/${invalidId}`).expect(400);
+      });
+    });
+  });
+
+  describe("PUT", () => {
+    describe("/api/blogs/:id", () => {
+      test("given valid id, correctly update blog", async () => {
+        const firstBlogInDB = await Blog.findOne({});
+        const validId = firstBlogInDB?.id;
+
+        const updatedData = {
+          title: "Updated Blog",
+          author: "Updated Author",
+          url: "https://example.com/updated-blog",
+        };
+
+        const response = await api
+          // @ts-ignore
+          .put(`${BLOGS_URI}/${validId}`)
+          .send(updatedData)
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
+
+        assertBlogShape(response.body);
+      });
+
+      test("given valid id, but no blog found, return 404", async () => {
+        const validId = generateValidMongooseId();
+        const updatedData = {
+          title: "Updated Blog",
+          author: "Updated Author",
+          url: "https://example.com/updated-blog",
+        };
+
+        await api.put(`${BLOGS_URI}/${validId}`).send(updatedData).expect(404);
+      });
+
+      test("given invalid id, return 400", async () => {
+        const invalidId = "invalid-id";
+
+        await api.put(`${BLOGS_URI}/${invalidId}`).send({}).expect(400);
+      });
     });
   });
 });
