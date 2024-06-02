67159c800f3812c2095197d3c31cbba1ae78efde
leo.vi <leonardo.vic3nte@gmail.com>
2024-06-02 11:52:32 -0300

    add tests for users endpoint for post

---
+++ part4/backend/controllers/userController.test.js
@@ -0,0 +1,98 @@
+import { beforeEach, describe, test, after } from "node:test";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import assert from "node:assert";
+import supertest from "supertest";
+import app from "../app.js";
+import User from "../models/userModel.js";
+
+const api = supertest(app);
+
+const USERS_URI = "/api/users";
+
+const dummyUserData = {
+  userWithName: {
+    name: "John Doe",
+    username: "johndoe",
+  },
+  userWithoutName: {
+    username: "randomdude",
+  },
+};
+
+const dummyHash = await bcrypt.hash("dummyPassword", 10);
+const dummyPassword = "dummyPassword";
+
+beforeEach(async () => {
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash("password123", 10);
+  const rootUser = new User({ username: "root", passwordHash });
+
+  await rootUser.save();
+});
+
+describe("USERS CONTROLLER", () => {
+  test("db initialize with a root user", async () => {
+    const usersFound = await User.find({});
+
+    assert(usersFound.length === 1);
+    assert(usersFound[0].username === "root");
+  });
+
+  test("correctly add default value for name", async () => {
+    const { username } = dummyUserData.userWithoutName;
+
+    const newUser = new User({ username, passwordHash: dummyHash });
+    const savedUser = await newUser.save();
+
+    assert(savedUser.name === "Anonymous");
+  });
+
+  describe("POST", () => {
+    test("create user given unique username", async () => {
+      const { username } = dummyUserData.userWithoutName;
+      const requestData = { username, password: dummyPassword };
+
+      await api
+        .post(USERS_URI)
+        .send(requestData)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+    });
+
+    test("returns 400 Bad Request if username is already found", async () => {
+      const username = "root";
+      const requestData = { username, password: dummyPassword };
+
+      const usersBefore = await User.find({});
+
+      await api
+        .post(USERS_URI)
+        .send(requestData)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+
+      const usersAfter = await User.find({});
+      assert(usersBefore.length === usersAfter.length);
+    });
+
+    test("returns 400 Bad Request if username is less than 3 chars", async () => {
+      const username = "lv";
+      const requestData = { username, password: dummyPassword };
+
+      const usersBefore = await User.find({});
+
+      await api
+        .post(USERS_URI)
+        .send(requestData)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+
+      const usersAfter = await User.find({});
+      assert(usersBefore.length === usersAfter.length);
+    });
+  });
+});
+
+after(() => mongoose.connection.close());
