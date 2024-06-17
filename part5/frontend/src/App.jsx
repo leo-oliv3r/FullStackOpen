dee799eca695a57a0716ae6d689604b46b23d3dd
leo.vi <leonardo.vic3nte@gmail.com>
2024-06-16 21:50:49 -0300

    add proxy to proper work with backend

--- part5/frontend/src/App.jsx
+++ part5/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import blogService from "./services/blogService";
+import Blog from "./components/Blog";
 
 function App() {
   const [blogs, setBlogs] = useState([]);
